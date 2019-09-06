b999e91200a0f83922ca1ca982fc2aa49d46d13b
wzzzrd86 <shane.weber86@gmail.com>
2019-09-06 09:11:23 -0700

    Update main.tf

--- server/main.tf
+++ server/main.tf
@@ -35,7 +35,7 @@ resource "aws_security_group" "default" {
 
 resource "aws_instance" "web" {
   ami           = var.ami
-  instance_type = "t2.medium"
+  instance_type = "t2.nano"
   count         = var.num_webs
 
   vpc_security_group_ids = [aws_security_group.default.id]
